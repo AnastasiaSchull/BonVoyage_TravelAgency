fb4337692a9185bb1a90045c92b1f0bf97a2a519
Nastia <nastasia.schull@gmail.com>
2024-07-08 20:48:06 +0200

    Add project files.

---
+++ BonVoyage_TravelAgency/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BonVoyage_TravelAgency.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
