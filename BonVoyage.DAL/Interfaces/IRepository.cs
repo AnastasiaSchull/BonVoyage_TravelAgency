78e24e21e8e2b03249d0c490a2a19f81d1af14c9
IrynaKonovalowa <iri.moummi@gmail.com>
2024-07-14 09:28:27 +0200

    Task5 Added IRepository and IUnitOfWork  to the DAL  in the Interfaces folder

---
+++ BonVoyage.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BonVoyage.DAL.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IQueryable<T>> GetAll();
+        Task<T> Get(int id);
+        Task Create(T item);
+        void Update(T item);
+        Task Delete(int id);
+    }
+}
---
+++ BonVoyage.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using BonVoyage.DAL.Entities;
+
+
+namespace BonVoyage.DAL.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Booking> Bookings { get; }
+        IRepository<CustomerPreference> CustomerPreferences { get; }
+        IRepository <FAQ> FAQs { get; }
+        IRepository<Flight> Flights { get; }
+        IRepository<Hotel> Hotels { get; }
+        IRepository<HotelPhoto> HotelPhotos { get; }
+        IRepository<Promotion> Promotions { get; }
+        IRepository<Review> Reviews { get; }
+        IRepository<Tour> Tours { get; }
+        IRepository<TourPhoto> TourPhotos { get; }
+        IRepository<User> Users { get; }
+
+        Task Save();
+    }
+}
