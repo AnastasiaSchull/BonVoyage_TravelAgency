80900766849c15a54c72f8c8d34176261193b246
IrynaKonovalowa <iri.moummi@gmail.com>
2024-07-16 12:59:52 +0200

    Task9 Created folder Infrastructure in BonVoyage.BLL Lasy loading enabled.

---
+++ BonVoyage.BLL/Infrastructure/BonVoyageContextExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using BonVoyage.DAL.EF;
+
+namespace BonVoyage.BLL.Infrastructure
+{
+    public static class BonVoyageContextExtensions
+    {
+        public static void AddBonVoyageContext(this IServiceCollection services, string connection)
+        {
+            services.AddDbContext<BonVoyageContext>(options => options.UseLazyLoadingProxies().UseSqlServer(connection));
+        }
+    }
+}
---
+++ BonVoyage.BLL/Infrastructure/UnitOfWorkServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using BonVoyage.DAL.Interfaces;
+using BonVoyage.DAL.Repositories;
+
+namespace BonVoyage.BLL.Infrastructure
+{
+    public static class UnitOfWorkServiceExtensions
+    {
+        public static void AddUnitOfWorkService(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, EFUnitOfWork>();
+        }
+    }
+}
