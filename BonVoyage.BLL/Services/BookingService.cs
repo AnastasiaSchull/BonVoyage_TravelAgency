f1411d35c2448691e2bebba63f72b078b977d227
Vladyslav <vlad.serdyuk.3456@gmail.com>
2024-07-24 17:47:46 +0300

    Task12 Implemented interfaces IBookingService and IUserService

---
+++ BonVoyage.BLL/Services/BookingService.cs
@@ -0,0 +1,44 @@
+﻿using BonVoyage.BLL.Interfaces;
+using BonVoyage.DAL.Entities;
+using BonVoyage.DAL.Interfaces;
+
+namespace BonVoyage.BLL.Services
+{
+    public class BookingService : IBookingService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BookingService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Booking>> GetAllBookingsAsync()
+        {
+            return await _unitOfWork.Bookings.GetAll();
+        }
+
+        public async Task<Booking> GetBookingByIdAsync(int id)
+        {
+            return await _unitOfWork.Bookings.Get(id);
+        }
+
+        public async Task CreateBookingAsync(Booking booking)
+        {
+            await _unitOfWork.Bookings.Create(booking);
+            await _unitOfWork.Save();
+        }
+
+        public async Task UpdateBookingAsync(Booking booking)
+        {
+            _unitOfWork.Bookings.Update(booking);
+            await _unitOfWork.Save();
+        }
+
+        public async Task DeleteBookingAsync(int id)
+        {
+            await _unitOfWork.Bookings.Delete(id);
+            await _unitOfWork.Save();
+        }
+    }
+}
---
+++ BonVoyage.BLL/Services/UserService.cs
@@ -0,0 +1,44 @@
+﻿using BonVoyage.BLL.Interfaces;
+using BonVoyage.DAL.Entities;
+using BonVoyage.DAL.Interfaces;
+
+namespace BonVoyage.BLL.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await _unitOfWork.Users.GetAll();
+        }
+
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            return await _unitOfWork.Users.Get(id);
+        }
+
+        public async Task CreateUserAsync(User user)
+        {
+            await _unitOfWork.Users.Create(user);
+            await _unitOfWork.Save();
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            _unitOfWork.Users.Update(user);
+            await _unitOfWork.Save();
+        }
+
+        public async Task DeleteUserAsync(int id)
+        {
+            await _unitOfWork.Users.Delete(id);
+            await _unitOfWork.Save();
+        }
+    }
+}
