70573ce3a3cd61ad14504eed8e3db314b55a458d
IrynaKonovalowa <iri.moummi@gmail.com>
2024-07-25 17:51:44 +0200

    Task17 Implemented interfaces HotelService and HotelPhotoService

---
+++ BonVoyage.BLL/Services/HotelPhotoService.cs
@@ -0,0 +1,69 @@
+﻿using BonVoyage.BLL.Interfaces;
+using BonVoyage.DAL.Interfaces;
+using BonVoyage.DAL.Entities;
+using BonVoyage.BLL.Infrastructure;
+using BonVoyage.BLL.DTOs;
+using AutoMapper;
+
+namespace BonVoyage.BLL.Services
+{
+    public class HotelPhotoService: IHotelPhotoService
+    {
+        IUnitOfWork Database { get; set; }
+
+        public HotelPhotoService(IUnitOfWork uow)
+        {
+            Database = uow;
+        }
+
+        public async Task CreateHotelPhotoAsync(HotelPhotoDTO hotelPhotoDTO )
+        {
+            var hotelPhoto = new HotelPhoto
+            {
+                HotelPhotoId  = hotelPhotoDTO.HotelPhotoId,
+                HotelId = hotelPhotoDTO.HotelId,
+                PhotoUrl = hotelPhotoDTO.PhotoUrl
+            };
+            await Database.HotelPhotos.Create(hotelPhoto);
+            await Database.Save();
+        }
+        public async Task UpdateHotelPhotoAsync(HotelPhotoDTO hotelPhotoDTO)
+        {
+            var hotelPhoto = new HotelPhoto
+            {
+                HotelPhotoId  = hotelPhotoDTO.HotelPhotoId,
+                HotelId = hotelPhotoDTO.HotelId,
+                PhotoUrl = hotelPhotoDTO.PhotoUrl
+            };
+            Database.HotelPhotos.Update(hotelPhoto);
+            await Database.Save();
+        }
+        public async Task DeleteHotelPhotoAsync(int id)
+        {
+            await Database.HotelPhotos.Delete(id);
+            await Database.Save();
+        }
+
+        public async Task<HotelPhotoDTO> GetHotelPhotoByIdAsync(int id)
+        {
+            var hotelPhoto = await Database.HotelPhotos.Get(id);
+            if (hotelPhoto  == null)
+                throw new ValidationException("Wrong hotel photo!", "");
+            return new HotelPhotoDTO
+            {
+                HotelPhotoId = hotelPhoto.HotelPhotoId,
+                HotelId = hotelPhoto.HotelId,
+                PhotoUrl = hotelPhoto.PhotoUrl,
+                Hotel = hotelPhoto.Hotel?.Name
+            };
+        }
+        // Automapper 
+        public async Task<IQueryable<HotelPhotoDTO>> GetAllHotelPhotosAsync()
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<HotelPhoto, HotelPhotoDTO>()
+            .ForMember("Hotel", opt => opt.MapFrom(c => c.Hotel.Name)));
+            var mapper = new Mapper(config);
+            return mapper.Map<IQueryable<HotelPhoto>, IQueryable<HotelPhotoDTO>>(await Database.HotelPhotos.GetAll());
+        }
+    }
+}
---
+++ BonVoyage.BLL/Services/HotelService.cs
@@ -0,0 +1,82 @@
+﻿using BonVoyage.BLL.Interfaces;
+using BonVoyage.DAL.Interfaces;
+using BonVoyage.DAL.Entities;
+using BonVoyage.BLL.Infrastructure;
+using BonVoyage.BLL.DTOs;
+using AutoMapper;
+
+
+namespace BonVoyage.BLL.Services
+{
+    public class HotelService: IHotelService
+    {
+        IUnitOfWork Database { get; set; }
+
+        public HotelService(IUnitOfWork uow)
+        {
+            Database = uow;
+        }
+
+        public async Task CreateHotelAsync(HotelDTO hotelDTO)
+        {
+            var hotel = new Hotel
+            {
+               HotelId = hotelDTO.HotelId,
+               Name = hotelDTO.Name,
+               Location = hotelDTO.Location,
+               PricePerNight = hotelDTO.PricePerNight,
+               StarRating = hotelDTO.StarRating,
+               HasSwimmingPool = hotelDTO.HasSwimmingPool,
+               TourId = hotelDTO.TourId
+               };
+           await Database.Hotels.Create(hotel);
+           await Database.Save();
+        }
+        public async Task UpdateHotelAsync(HotelDTO hotelDTO)
+        {
+            var hotel = new Hotel
+            {
+                HotelId = hotelDTO.HotelId,
+                Name = hotelDTO.Name,
+                Location = hotelDTO.Location,
+                PricePerNight = hotelDTO.PricePerNight,
+                StarRating = hotelDTO.StarRating,
+                HasSwimmingPool = hotelDTO.HasSwimmingPool,
+                TourId = hotelDTO.TourId
+            };
+            Database.Hotels.Update(hotel);
+            await Database.Save();
+        }
+        public async Task DeleteHotelAsync(int id)
+        {
+            await Database.Hotels.Delete(id);
+            await Database.Save();
+        }
+
+        public async Task<HotelDTO> GetHotelByIdAsync(int id)
+        {
+            var hotel = await Database.Hotels.Get(id);
+            if (hotel  == null)
+                throw new ValidationException("Wrong hotel!", "");
+            return new HotelDTO
+            {
+                HotelId = hotel.HotelId,
+                Name = hotel.Name,
+                Location = hotel.Location,
+                PricePerNight = hotel.PricePerNight,
+                StarRating = hotel.StarRating,
+                HasSwimmingPool = hotel.HasSwimmingPool,
+                TourId = hotel.TourId,
+                Tour = hotel.Tour?.Title
+            };
+        }
+        // Automapper 
+        public async Task<IQueryable<HotelDTO>> GetAllHotelsAsync()
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Hotel, HotelDTO>()
+            .ForMember("Tour", opt => opt.MapFrom(c => c.Tour.Title)));
+            var mapper = new Mapper(config);
+            return mapper.Map<IQueryable<Hotel>, IQueryable<HotelDTO>>(await Database.Hotels.GetAll());
+        }
+    }
+}
