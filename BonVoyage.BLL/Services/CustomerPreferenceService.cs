ae615a73487c61fbf66bdbdfe0e4c360a9fc3303
Nastia <nastasia.schull@gmail.com>
2024-07-25 13:52:32 +0200

    Task 14 Implemented interfaces IFAQService and ICustomerPreferenceService
    Added FAQ and customer preference services. These services allow for adding, updating, deleting, and retrieving details related to FAQs and user preferences.

---
+++ BonVoyage.BLL/Services/CustomerPreferenceService.cs
@@ -0,0 +1,47 @@
+﻿using BonVoyage.BLL.Interfaces;
+using BonVoyage.DAL.Entities;
+using BonVoyage.DAL.Interfaces;
+using BonVoyage.DAL.Repositories;
+
+
+namespace BonVoyage.BLL.Services
+{
+	public class CustomerPreferenceService : ICustomerPreferenceService
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public CustomerPreferenceService(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public async Task<IEnumerable<CustomerPreference>> GetAllPreferencesAsync()
+		{
+			return await _unitOfWork.CustomerPreferences.GetAll();
+		}
+
+		public async Task<CustomerPreference> GetPreferenceByIdAsync(int id)
+		{
+			return await _unitOfWork.CustomerPreferences.Get(id);
+		}
+
+		public async Task CreatePreferenceAsync(CustomerPreference preference)
+		{
+			await _unitOfWork.CustomerPreferences.Create(preference);
+			await _unitOfWork.Save();
+		}
+
+		public async Task UpdatePreferenceAsync(CustomerPreference preference)
+		{
+			_unitOfWork.CustomerPreferences.Update(preference);
+			await _unitOfWork.Save();
+		}
+
+		public async Task DeletePreferenceAsync(int id)
+		{
+			await _unitOfWork.CustomerPreferences.Delete(id);
+			await _unitOfWork.Save();
+		}
+
+	}
+}
---
+++ BonVoyage.BLL/Services/FAQService.cs
@@ -0,0 +1,44 @@
+﻿using BonVoyage.BLL.Interfaces;
+using BonVoyage.DAL.Entities;
+using BonVoyage.DAL.Interfaces;
+
+namespace BonVoyage.BLL.Services
+{
+	public class FAQService : IFAQService
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public FAQService(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public async Task<IEnumerable<FAQ>> GetAllFAQsAsync()
+		{
+			return await _unitOfWork.FAQs.GetAll();
+		}
+
+		public async Task<FAQ> GetFAQByIdAsync(int id)
+		{
+			return await _unitOfWork.FAQs.Get(id);
+		}
+
+		public async Task CreateFAQAsync(FAQ faq)
+		{
+			await _unitOfWork.FAQs.Create(faq);
+			await _unitOfWork.Save();
+		}
+
+		public async Task UpdateFAQAsync(FAQ faq)
+		{
+			_unitOfWork.FAQs.Update(faq);
+			await _unitOfWork.Save();
+		}
+
+		public async Task DeleteFAQAsync(int id)
+		{
+			await _unitOfWork.FAQs.Delete(id);
+			await _unitOfWork.Save();
+		}
+	}
+}
