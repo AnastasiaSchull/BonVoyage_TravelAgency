c0cb2126db4e95e81686057169d44d9dc0c3a2cd
IrynaKonovalowa <iri.moummi@gmail.com>
2024-07-27 18:28:37 +0200

    Task22 Changed FlightDTO and PromotionDTO

--- BonVoyage.BLL/DTOs/FlightDTO.cs
+++ BonVoyage.BLL/DTOs/FlightDTO.cs
@@ -5,13 +5,15 @@ namespace BonVoyage.BLL.DTOs
     public class FlightDTO
     {
         public int FlightId { get; set; }
-        public string DepartureLocation { get; set; }
-        public string ArrivalLocation { get; set; }
+        public string? DepartureLocation { get; set; }
+        public string? ArrivalLocation { get; set; }
         public decimal Price { get; set; }
-        public string Airline { get; set; }
-        public string FlightNumber { get; set; }
+        public string? Airline { get; set; }
+        public string? FlightNumber { get; set; }
         public DateTime DepartureTime { get; set; }
         public DateTime ArrivalTime { get; set; }
         public TimeSpan Duration { get; set; }
+        public int TourId { get; set; }
+        public string? Tour { get; set; }
     }
 }
--- BonVoyage.BLL/DTOs/PromotionDTO.cs
+++ BonVoyage.BLL/DTOs/PromotionDTO.cs
@@ -5,7 +5,7 @@ namespace BonVoyage.BLL.DTOs
     public class PromotionDTO
     {
         public int PromotionId { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public decimal Discount { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
