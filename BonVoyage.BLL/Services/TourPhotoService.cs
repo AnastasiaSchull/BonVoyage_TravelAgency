d82fcb207cdfcf37b96b4d1ad550ddc1120be515
IrynaKonovalowa <iri.moummi@gmail.com>
2024-07-28 19:44:52 +0200

    Task28 Added TourService and TourPhotoService

---
+++ BonVoyage.BLL/Services/TourPhotoService.cs
@@ -0,0 +1,69 @@
+﻿using BonVoyage.BLL.Interfaces;
+using BonVoyage.DAL.Interfaces;
+using BonVoyage.DAL.Entities;
+using BonVoyage.BLL.Infrastructure;
+using BonVoyage.BLL.DTOs;
+using AutoMapper;
+
+namespace BonVoyage.BLL.Services
+{
+    public class TourPhotoService: ITourPhotoService
+    {
+        IUnitOfWork Database { get; set; }
+
+        public TourPhotoService(IUnitOfWork uow)
+        {
+            Database = uow;
+        }
+
+        public async Task CreateTourPhotoAsync(TourPhotoDTO tourPhotoDTO)
+        {
+            var tourPhoto = new TourPhoto
+            {
+                TourPhotoId = tourPhotoDTO.TourPhotoId,
+                TourId = tourPhotoDTO.TourId,
+                PhotoUrl = tourPhotoDTO.PhotoUrl
+            };
+            await Database.TourPhotos.Create(tourPhoto);
+            await Database.Save();
+        }
+        public async Task UpdateTourPhotoAsync(TourPhotoDTO tourPhotoDTO)
+        {
+            var tourPhoto = new TourPhoto
+            {
+                TourPhotoId = tourPhotoDTO.TourPhotoId,
+                TourId = tourPhotoDTO.TourId,
+                PhotoUrl = tourPhotoDTO.PhotoUrl
+            };
+            Database.TourPhotos.Update(tourPhoto);
+            await Database.Save();
+        }
+        public async Task DeleteTourPhotoAsync(int id)
+        {
+            await Database.TourPhotos.Delete(id);
+            await Database.Save();
+        }
+
+        public async Task<TourPhotoDTO> GetTourPhotoByIdAsync(int id)
+        {
+            var tourPhoto = await Database.TourPhotos.Get(id);
+            if (tourPhoto  == null)
+                throw new ValidationException("Wrong tour photo!", "");
+            return new TourPhotoDTO
+            {
+                TourPhotoId = tourPhoto.TourPhotoId,
+                TourId = tourPhoto.TourId,
+                PhotoUrl = tourPhoto.PhotoUrl,
+                Tour = tourPhoto.Tour?.Title
+            };
+        }
+        // Automapper 
+        public async Task<IQueryable<TourPhotoDTO>> GetAllTourPhotosAsync()
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<TourPhoto, TourPhotoDTO>()
+            .ForMember("Tour", opt => opt.MapFrom(c => c.Tour.Title)));
+            var mapper = new Mapper(config);
+            return mapper.Map<IQueryable<TourPhoto>, IQueryable<TourPhotoDTO>>(await Database.TourPhotos.GetAll());
+        }
+    }
+}
---
+++ BonVoyage.BLL/Services/TourService.cs
@@ -0,0 +1,82 @@
+﻿using BonVoyage.BLL.Interfaces;
+using BonVoyage.DAL.Interfaces;
+using BonVoyage.DAL.Entities;
+using BonVoyage.BLL.Infrastructure;
+using BonVoyage.BLL.DTOs;
+using AutoMapper;
+
+namespace BonVoyage.BLL.Services
+{
+    public class TourService: ITourService
+    {
+        IUnitOfWork Database { get; set; }
+
+        public TourService(IUnitOfWork uow)
+        {
+            Database = uow;
+        }
+        public async Task CreateTourAsync(TourDTO tourDTO)
+        {
+            var tour = new Tour
+            {
+                TourId = tourDTO.TourId,
+                Title = tourDTO.Title,
+                Description = tourDTO.Description,
+                Duration = tourDTO.Duration,
+                Price = tourDTO.Price,
+                Country = tourDTO.Country,
+                Route = tourDTO.Route,
+                StartDate = tourDTO.StartDate,
+                EndDate = tourDTO.EndDate
+            };
+            await Database.Tours.Create(tour);
+            await Database.Save();
+        }
+        public async Task UpdateTourAsync(TourDTO tourDTO)
+        {
+            var tour = new Tour
+            {
+                TourId = tourDTO.TourId,
+                Title = tourDTO.Title,
+                Description = tourDTO.Description,
+                Duration = tourDTO.Duration,
+                Price = tourDTO.Price,
+                Country = tourDTO.Country,
+                Route = tourDTO.Route,
+                StartDate = tourDTO.StartDate,
+                EndDate = tourDTO.EndDate
+            };
+            Database.Tours.Update(tour);
+            await Database.Save();
+        }
+        public async Task DeleteTourAsync(int id)
+        {
+            await Database.Tours.Delete(id);
+            await Database.Save();
+        }
+        public async Task<TourDTO> GetTourByIdAsync(int id)
+        {
+            var tour = await Database.Tours.Get(id);
+            if (tour  == null)
+                throw new ValidationException("Wrong tour!", "");
+            return new TourDTO
+            {
+                TourId = tour.TourId,
+                Title = tour.Title,
+                Description = tour.Description,
+                Duration = tour.Duration,
+                Price = tour.Price,
+                Country = tour.Country,
+                Route = tour.Route,
+                StartDate = tour.StartDate,
+                EndDate = tour.EndDate
+            };
+        }
+        // Automapper 
+        public async Task<IQueryable<TourDTO>> GetAllToursAsync()
+        {
+            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Tour, TourDTO>()).CreateMapper();
+            return mapper.Map<IQueryable<Tour>, IQueryable<TourDTO>>(await Database.Tours.GetAll());
+        }
+    }
+}
