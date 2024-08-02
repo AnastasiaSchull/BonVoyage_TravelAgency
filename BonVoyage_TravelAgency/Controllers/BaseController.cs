d7bc6e181b45c3f155a01b18c4828bb85b495159
Nastia <nastasia.schull@gmail.com>
2024-08-03 00:34:15 +0200

    Task 34 Added BaseController with ChangeCulture method
    ChangeCulture allows the user to change the interface language and saves this choice in a cookie for future use. This complements the functionality of the CultureAttribute, which automatically applies the selected language to all controller actions.

---
+++ BonVoyage_TravelAgency/Controllers/BaseController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BonVoyage_TravelAgency.Filters;
+using Microsoft.Extensions.Localization;
+
+namespace BonVoyage_TravelAgency.Controllers
+{
+    // BaseController applies the CultureAttribute filter (by using the [Culture] attribute), inherits from Controller. All other controllers will inherit from BaseController
+
+    [Culture]
+    public class BaseController : Controller
+    {
+        public ActionResult ChangeCulture(string lang)
+        {
+            string? returnUrl = HttpContext.Session.GetString("path") ?? "/Home/Index";
+
+            List<string> cultures = new List<string>() { "en", "uk", "fr", "sk" };
+            if (!cultures.Contains(lang))
+            {
+                lang = "en"; // default language
+            }
+
+            CookieOptions option = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(10)
+            };
+            Response.Cookies.Append("lang", lang, option);
+            return Redirect(returnUrl);
+        }
+    }
+}
