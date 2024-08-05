52f95a5616e7442f4be97c3fbc7ff95111209e07
IrynaKonovalowa <iri.moummi@gmail.com>
2024-08-05 18:49:55 +0200

    Task 37 React.AspNet included to the project. Startup.cs configurated.

---
+++ BonVoyage_TravelAgency/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using React.AspNet;
+
+namespace BonVoyage_TravelAgency
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMemoryCache();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();            
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseReact(config => { });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
