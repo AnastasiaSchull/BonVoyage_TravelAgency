0beb788cdbddba9e298c295fb06a09d837a83674
Nastia <nastasia.schull@gmail.com>
2024-08-18 12:16:57 +0200

    Task 56 Created a new MessageRepository for chat functionality
    And implemented IUserRepository interface separate from the generic IRepository to accommodate chat-specific methods.
    Added new methods in UserRepository such as GetUserByUsernameAsync and GetUserByConnectionIdAsync to support chat operations.

---
+++ BonVoyage.DAL/Repositories/MessageRepository.cs
@@ -0,0 +1,29 @@
+﻿using BonVoyage.DAL.EF;
+using BonVoyage.DAL.Entities;
+using BonVoyage.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+public class MessageRepository : IMessageRepository
+{
+    private readonly BonVoyageContext db;
+
+    public MessageRepository(BonVoyageContext context)
+    {
+        db = context;
+    }
+
+    public async Task AddMessageAsync(Message message)
+    {
+         db.Messages.Add(message);
+    }
+
+    public async Task<List<Message>> GetAllMessagesAsync()
+    {
+        return await db.Messages.ToListAsync();
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await db.SaveChangesAsync();
+    }
+}
