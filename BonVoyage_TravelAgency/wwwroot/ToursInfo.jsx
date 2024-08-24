e2033719a7be27de7aac71fea6a5d1e2175fe96b
IrynaKonovalowa <iri.moummi@gmail.com>
2024-08-24 17:41:43 +0200

    Task 62 Component ToursInfo changed. Transfer tourId to Booking. Format of date changed.

--- BonVoyage_TravelAgency/wwwroot/ToursInfo.jsx
+++ BonVoyage_TravelAgency/wwwroot/ToursInfo.jsx
@@ -1,4 +1,18 @@
 ﻿class ToursInfo extends React.Component {
+    constructor(props) {
+        super(props);        
+        this.linkToBooking = this.linkToBooking.bind(this);   
+        this.dateFofmat = this.dateFofmat.bind(this); 
+    }
+
+    linkToBooking(tourId) {
+        let link = 'https://localhost:7079/Booking/Create/' + tourId;
+        return link;
+    }
+    dateFofmat(date) {
+        let newFormatDate = new Date(date).toLocaleDateString();        
+        return newFormatDate;
+    }
     
     render() {
         return <div class="col-md-4">
@@ -9,17 +23,17 @@
                     <p class="card-text">{this.props.tour.description}</p>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">Duration: {this.props.tour.duration} days</li>
-                        <li class="list-group-item">Price: {this.props.tour.price}</li>
+                        <li class="list-group-item">Price: ${this.props.tour.price}</li>
                         <li class="list-group-item">Country: {this.props.tour.country}</li>
                         <li class="list-group-item">Route: {this.props.tour.route}</li>
                     </ul>
                     <div class="card-footer">
-                        <small class="text-muted">Start Date: {this.props.tour.startDate}</small>
+                        <small class="text-muted">Start Date: {this.dateFofmat(this.props.tour.startDate)}</small>
                         <br />
-                        <small class="text-muted">End Date: {this.props.tour.endDate}</small>
+                        <small class="text-muted">End Date: {this.dateFofmat(this.props.tour.endDate)}</small>
                     </div>
                     <div>
-                        <a href="#"/*"https://localhost:7079/Booking/Create" */ className="nav-link d-inline" target="_blank" rel="noopener noreferrer">Book</a>
+                        <a href={this.linkToBooking(this.props.tour.tourId)} className="nav-link d-inline" target="_blank" rel="noopener noreferrer">Book</a>
                     </div>
                </div>
             </div>
