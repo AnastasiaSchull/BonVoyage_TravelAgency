1c783e00c3b6a929e580a4d9249c094aaa7fdb2a
Nastia <nastasia.schull@gmail.com>
2024-08-28 19:10:45 +0200

    Task 66 Modified chat functionality to automatically connect authenticated users and handle their usernames based on session data.

--- BonVoyage_TravelAgency/Controllers/ChatController.cs
+++ BonVoyage_TravelAgency/Controllers/ChatController.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BonVoyage_TravelAgency.Controllers
 {
     public class ChatController : Controller
-    {
+    {      
         public IActionResult Index()
         {
             return View();
