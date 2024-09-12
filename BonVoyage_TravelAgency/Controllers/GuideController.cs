103bec3d287f3082e2d0db647b6b6c693f5c6304
Vladyslav <vlad.serdyuk.3456@gmail.com>
2024-09-12 18:31:25 +0300

    Task 78 Added guide pages (insurance, news, tips etc)

---
+++ BonVoyage_TravelAgency/Controllers/GuideController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BonVoyage_TravelAgency.Controllers
+{
+    public class GuideController : Controller
+    {
+        public IActionResult WhereToGo()
+        {
+            return View();
+        }
+
+        public IActionResult TravelInsurance()
+        {
+            return View();
+        }
+
+        public IActionResult TravelNews()
+        {
+            return View();
+        }
+
+        public IActionResult TipsForTourists()
+        {
+            return View();
+        }
+
+        public IActionResult CoronavirusTests()
+        {
+            return View();
+        }
+    }
+}
