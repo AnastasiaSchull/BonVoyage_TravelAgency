701a3c76fffb598c5533773203132dfa1d1acb7e
IrynaKonovalowa <iri.moummi@gmail.com>
2024-10-20 22:13:04 +0200

    Task 101 Filtration bookings

---
+++ BonVoyage_TravelAgency/Models/BookingFilterModel.cs
@@ -0,0 +1,17 @@
+﻿using BonVoyage.BLL.DTOs;
+
+namespace BonVoyage_TravelAgency.Models
+{
+    public class BookingFilterModel
+    {
+        public IEnumerable<BookingDTO>? Bookings { get; set; } = new List<BookingDTO>();        
+        public string? Status { get; set; }        
+        public string? User { get; set; }
+        public BookingFilterModel(IEnumerable<BookingDTO> bookings, string status, string user)
+        {
+            Bookings = bookings;
+            Status = status;
+            User = user;
+        }
+    }
+}
