87b816151131063cfbb16ff9e726501e565d7342
Nastia <nastasia.schull@gmail.com>
2024-10-26 23:28:01 +0200

    Task 109 Implemented CharterGreece Page and Controller

---
+++ BonVoyage_TravelAgency/Controllers/ChartersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BonVoyage_TravelAgency.Controllers
+{
+    public class ChartersController : BaseController
+    {
+        //для отображения страницы с чартерами в Грецию
+        public IActionResult Greece()
+        {
+            return View("CharterGreece");
+        }
+    }
+}
