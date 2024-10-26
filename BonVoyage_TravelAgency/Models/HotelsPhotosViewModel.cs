ad6f2774d6b02f59985a4eb30428ea7dc1270cec
IrynaKonovalowa <139651410+IrynaKonovalowa@users.noreply.github.com>
2024-10-26 21:15:15 +0200

    Task 107 HotelsPhotosViewModel added, View FilteredHotelsByTours added, Layout changed.

---
+++ BonVoyage_TravelAgency/Models/HotelsPhotosViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BonVoyage_TravelAgency.Models
+{
+    public class HotelsPhotosViewModel
+    {
+        public IEnumerable<BonVoyage.BLL.DTOs.HotelDTO>? Hotels { get; set; }
+        public IEnumerable<BonVoyage.BLL.DTOs.HotelPhotoDTO>? HotelsPhotos { get; set; }
+
+        public string Tour { get; set; }
+    }
+}
