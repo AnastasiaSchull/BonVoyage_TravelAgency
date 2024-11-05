81d756606cb7d4dcdd93c388bf4573b4f55ba513
Nastia <nastasia.schull@gmail.com>
2024-11-05 18:51:34 +0100

    122 Task Added user Bookings display on Profile page.
    Added UserProfileViewModel to support display of User profile and Bookings on View.  Injected  BookingService in AccountController.

---
+++ BonVoyage_TravelAgency/Models/UserProfileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BonVoyage.BLL.DTOs;
+
+namespace BonVoyage_TravelAgency.Models
+{
+    public class UserProfileViewModel
+    {
+        public UserDTO User { get; set; } 
+        public List<BookingDTO> Bookings { get; set; } 
+
+        public UserProfileViewModel()
+        {
+            User = new UserDTO();
+            Bookings = new List<BookingDTO>();
+        }
+    }
+
+}
